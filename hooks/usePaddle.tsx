6ed36943aeae8e58d44aadea4e537c2aa59c5313
Nirodya Gamage <nirodyagamage@gmail.com>
2024-05-06 15:02:08 +0530

    Update usePaddle.tsx

--- hooks/usePaddle.tsx
+++ hooks/usePaddle.tsx
@@ -6,9 +6,9 @@ import { useEffect, useState } from "react";
 export default function usePaddle() {
   const [paddle, setPaddle] = useState<Paddle>();
   useEffect(() => {
-    // console.log(process.env.NEXT_PUBLIC_PADDLE_ENV);
-    // console.log(process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN);
-    // console.log(process.env.NEXT_PUBLIC_PADDLE_SELLER_ID);
+    console.log(process.env.NEXT_PUBLIC_PADDLE_ENV);
+    console.log(process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN);
+    console.log(process.env.NEXT_PUBLIC_PADDLE_SELLER_ID);
     initializePaddle({
       environment: process.env.NEXT_PUBLIC_PADDLE_ENV,
       // token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN!,
