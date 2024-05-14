237e44c9380b02592ddfc6fd682eed88ccd0b0d0
Nirodya Gamage <nirodyagamage@gmail.com>
2024-05-14 16:21:26 +0530

    Fixes

--- utils/supabase/admin.ts
+++ utils/supabase/admin.ts
@@ -1,5 +1,4 @@
-import { toDateTime } from '@/utils/helpers';
-import { stripe } from '@/utils/stripe/config';
+import { stripe } from '../../utils/stripe/config';
 import { createClient } from '@supabase/supabase-js';
 import Stripe from 'stripe';
 import type { Database, Tables, TablesInsert } from 'types_db';
